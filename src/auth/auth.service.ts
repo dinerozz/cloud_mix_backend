af55733c2776f0d4452cbb4c796746df547f1835
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2023-10-24 20:49:54 +0600

    fix: access token expiration time

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -112,7 +112,7 @@ export class AuthService {
     };
     const refreshTokenPayload = { sub: user.id, jti: uuid() };
     const accessToken = this.jwtService.sign(accessTokenPayload, {
-      expiresIn: "3m",
+      expiresIn: "30m",
     });
     const refreshToken = this.jwtService.sign(refreshTokenPayload, {
       expiresIn: "7d",
