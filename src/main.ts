f8982d3879f2d9f6a677adc653067b5435871ed5
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2023-10-26 13:52:40 +0600

    fix: cors options

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*",
+    origin: process.env.ORIGIN_URL || "http://localhost",
     credentials: true,
   });
 
