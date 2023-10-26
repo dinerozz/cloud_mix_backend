e90955a36ff88037b2827eab06f9f504f5166a68
Murat Aznabakiyev <m.aznabakiyev@petrel.ai>
2023-10-26 13:44:58 +0600

    fix: cors options

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "https://cloud-mix-dinerozz.vercel.app",
-        "https://cloud-mix.vercel.app",
-      ];
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
     credentials: true,
   });
 
